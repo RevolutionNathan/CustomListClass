d9809dcce636f0400e2b572c181ae33ef23cf8d5
Nathan Whitcomb <nathanwhitcomb@MacBook-Pro.local>
2017-05-15 08:05:39 -0500

    custom list class

---
+++ CustomListCLass/List.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomListCLass
+{
+    public class CustomList<T> : IEnumerable
+    {
+        private int length;
+        T[] items;
+
+        public CustomList()
+        {
+            length = 0;
+            items = new T[length];
+        }
+
+      
+
+        
+        public IEnumerator GetEnumerator()
+        {
+           for (int i =0; i < length; i++)
+            {
+                yield return items[i];
+            }
+        }
+        public T this [int i]
+        {
+            get 
+            {
+                return items[i];
+            }
+            set
+            {
+                items[i] = value;
+            }
+        }
+
+        public void CheckTheIndex()
+        {
+
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return (IEnumerator)GetEnumerator(); 
+        }
+        public int Counter()
+        {
+            length = 0;
+            foreach (T t in items)
+            {
+                length++;
+            }
+            return length;
+        }
+
+        public int Count
+        {
+            get
+            {
+                return length;
+            }
+
+        }
+
+        public void Add(T itemAdded)
+        {
+            T[] temporaryItems = new T[length + 1];
+            for (int i = 0; i < length; i++)
+                {
+                    temporaryItems[i] = items[i];
+                }
+            temporaryItems[length] = itemAdded;
+            items = temporaryItems;
+            length++;
+        }
+
+        public bool Remove(T itemRemoved)
+        {
+            Counter();
+            T[] temporaryItems = new T[length - 1];
+            T[] trash = new T[1];
+            int fillCounter = 0;
+            for (int i = 0; i < length; i++)
+            {
+                if (i < items.Length)
+                {
+                    if (items[i].Equals(itemRemoved))
+                    {
+                        trash[0] = items[i];
+                    }
+                    else
+                    {
+                        temporaryItems[fillCounter] = items[i];
+                        fillCounter++;
+                    }
+                }
+                   
+            }
+            if (trash[0].Equals(itemRemoved))
+            {
+                items = temporaryItems;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        public override string ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < length; i++)
+            {
+                builder.Append(items[i]);
+                builder.Append(",");
+            }
+            builder.Length -= 1;
+            string itemsAsString = builder.ToString();
+           
+            return itemsAsString;
+        }
+
+        public static CustomList<T> operator+ (CustomList<T> itemsA, CustomList<T> itemsB)
+        {
+          
+            CustomList<T> temporaryItems = new CustomList<T>();
+
+            for (int i = 0; i < itemsA.length; i++)
+            {
+                temporaryItems.Add(itemsA[i]);
+            }
+            for (int i = 0; i < itemsA.length; i++)
+            {
+                temporaryItems.Add(itemsB[i]);
+            }
+            return temporaryItems;
+        }
+
+        public static CustomList<T> operator- (CustomList<T> items, CustomList<T> itemsSubtracted)
+        {
+            for (int i = 0; i < itemsSubtracted.length; i++)
+            {
+                items.Remove(itemsSubtracted[i]);
+            }
+            return items;
+        }
+        
+          public CustomList<T> Zip(CustomList<T> itemsAdded)
+        {
+            CustomList<T> temporaryItems = new CustomList<T>();
+            int counter = (length <= itemsAdded.length) ? length : itemsAdded.length;
+            for (int i = 0; i < counter; i++)
+            {
+                temporaryItems.Add(items[i]);
+                temporaryItems.Add(itemsAdded[i]);
+            }
+            return temporaryItems;
+        }
+        public int? GiveValueGetIndex(T valueWhoseIndexSought)
+        {
+            int i = 0;
+            for (i = 0; i < length; i++)
+            {
+                if (valueWhoseIndexSought.Equals(items[i]))
+                {
+                    return i;
+                }
+            }
+            return null;
+        }
+        public T GiveIndexGetValue(int indexWhoseValueSought)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                if (indexWhoseValueSought.Equals(i))
+                {
+                    return items[i];
+                }
+            }
+            return default(T);
+        }
+
+
+
+
+    }
+}
---
+++ TestCustomListClass/UnitTest1.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomListCLass;
+
+namespace TestCustomListClass
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        [TestMethod]
+        public void GiveValueGetIndex_ItemsInList_MatchExpectedIndex()
+        {
+            CustomList<int> list = new CustomList<int>() { 0, 1, 2, 3, 4, 5 };
+            int valueGiven = 5;
+            int? indexResulting;
+            int? indexExpected = 5;
+
+            indexResulting = list.GiveValueGetIndex(valueGiven);
+
+            Assert.AreEqual(indexExpected, indexResulting);
+        }
+        [TestMethod]
+        public void GiveValueGetIndex_ItemsInList_NotNull()
+        {
+            CustomList<int> list = new CustomList<int>() { 0, 1, 2, 3, 4, 5 };
+            int valueGiven = 5;
+            int? indexResulting;
+
+            indexResulting = list.GiveValueGetIndex(valueGiven);
+
+            Assert.IsNotNull(indexResulting);
+        }
+
+        [TestMethod]
+        public void GiveValueGetIndex_ItemsInList_WrongValueIsNull()
+        {
+            CustomList<int> list = new CustomList<int>() { 0, 1, 2, 3, 4, 5 };
+            int valueGiven = 6;
+            int? indexResulting;
+
+            indexResulting = list.GiveValueGetIndex(valueGiven);
+
+            Assert.IsNull(indexResulting);
+        }
+
+
+        [TestMethod]
+        public void GiveValueGetIndex_ItemsInList_ReturnsFirstInstanceOf()
+        {
+            CustomList<int> list = new CustomList<int>() { 0, 5, 2, 3, 4, 5 };
+            int valueGiven = 5;
+            int indexExpected = 1;
+            int? indexResulting;
+
+            indexResulting = list.GiveValueGetIndex(valueGiven);
+
+            Assert.AreEqual(indexResulting, indexExpected);
+        }
+
+        [TestMethod]
+        public void GiveIndexGetValue_ItemsInList_IsNotNull()
+        {
+            CustomList<int> list = new CustomList<int>() { 0, 5, 2, 3, 4, 5 };
+            int indexGiven = 1;
+            int valueResulting;
+
+            valueResulting = list.GiveIndexGetValue(indexGiven);
+
+            Assert.IsNotNull(valueResulting);
+        }
+
+        public void GiveIndexGetValue_ItemsInList_IstNull()
+        {
+            CustomList<int> list = new CustomList<int>() { 0, 5, 2, 3, 4, 5 };
+            int indexGiven = 7;
+            int valueResulting;
+
+            valueResulting = list.GiveIndexGetValue(indexGiven);
+
+            Assert.IsNull(valueResulting);
+        }
+
+        [TestMethod]
+        public void Count_ItemsInList_ReturnsCount()
+        {
+            CustomList<int> list = new CustomList<int>() { 0, 1, 2, 3, 4, 5 };
+
+            int result;
+            int expected = 6;
+            //arrange
+
+            result = list.Count;
+            //act
+
+            Assert.AreEqual(result, expected);
+            //assert 
+        }
+
+        [TestMethod]
+        public void ToString_ChangeArrayToString_MatchesString()
+        {
+            CustomList<int> list = new CustomList<int>() { 1, 2, 3, 4, 5 };
+            string result;
+            string expected = "1,2,3,4,5";
+
+            result = list.ToString();
+
+            Assert.AreEqual(result, expected);
+        }
+        [TestMethod]
+        public void ToString_ChangeArrayToString_GiantInt()
+        {
+            CustomList<int> list = new CustomList<int>() { 999999999, 2, 3, 4, 5 };
+            string result;
+            string expected = "999999999,2,3,4,5";
+
+            result = list.ToString();
+
+            Assert.AreEqual(result, expected);
+        }
+        [TestMethod]
+        public void ToString_ChangeArrayToString_ContainsANegative()
+        {
+            CustomList<int> list = new CustomList<int>() { -16, 2, 3, 4, 5 };
+            string result;
+            string expected = "-16,2,3,4,5";
+
+            result = list.ToString();
+
+            Assert.AreEqual(result, expected);
+        }
+        [TestMethod]
+        public void Add_ItemInList_ReturnsAccurateCount()
+        {
+            CustomList<int> list = new CustomList<int>();
+            int result;
+            int expected = 1;
+            int addedItem = 5;
+
+            list.Add(addedItem);
+            result = list.Counter();
+
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void Add_ItemInList_CheckIndexOfValue()
+        {
+            CustomList<int> list = new CustomList<int>();
+            int? resultingIndex;
+            int expectedIndex = 0;
+            int addedItem = 5;
+
+            list.Add(addedItem);
+            resultingIndex = list.GiveValueGetIndex(addedItem);
+
+            Assert.AreEqual(resultingIndex, expectedIndex);
+
+        }
+
+
+        [TestMethod]
+        public void Add_ItemInList_CheckValueAtIndex()
+        {
+            CustomList<int> list = new CustomList<int>();
+            int resultValue;
+            int expectedValue = 5;
+            int addedItem = 5;
+
+
+            list.Add(addedItem);
+            resultValue = list[0];
+
+            Assert.AreEqual(resultValue, expectedValue);
+        }
+
+        [TestMethod]
+        public void Remove_ItemFromList_ReturnsMatchingIndices()
+        {
+            CustomList<int> list = new CustomList<int>() { 0, 1, 2, 3, 4, 5 };
+
+            int removedItem = 2;
+            int expected = 5;
+
+            list.Remove(removedItem);
+            int result = list[4];
+
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void Remove_ItemFromList_ReturnsAccurateCount()
+        {
+            CustomList<int> list = new CustomList<int>() { 0, 1, 2, 3, 4, 5 };
+            int resultOfCount;
+            int expectedCount = 5;
+            int itemRemoved = 2;
+
+
+            list.Remove(itemRemoved);
+            resultOfCount = list.Counter();
+
+            Assert.AreEqual(resultOfCount, expectedCount);
+        }
+
+        [TestMethod]
+        public void Remove_RemoveMultipleObjectsFromList_ReturnLengthOfListWithRemovedElements()
+        {
+            CustomList<int> list = new CustomList<int>();
+            //Arrange
+            int number = 1;
+            int biggerNumber = 2;
+            int biggestNumber = 3;
+            int expected = 1;
+            int result;
+
+            //Act
+            list.Add(number);
+            list.Add(biggerNumber);
+            list.Add(biggestNumber);
+            list.Remove(biggerNumber);
+            list.Remove(biggestNumber);
+            result = list.Counter();
+
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Plus_Overload_CheckValueAtIndex()
+        {
+            CustomList<int> listA = new CustomList<int>() { 1, 2, 3 };
+            CustomList<int> listB = new CustomList<int>() { 1, 2, 3 };
+
+            int expectedValue = 1;
+            int resultingValue;
+            CustomList<int> list = new CustomList<int>();
+
+            list = listA + listB;
+
+            resultingValue = list[3];
+
+            Assert.AreEqual(expectedValue, resultingValue);
+        }
+
+        [TestMethod]
+        public void Plus_Overload_AddThreeLists()
+        {
+            CustomList<int> listA = new CustomList<int>() { 0, 1, 2 };
+            CustomList<int> listB = new CustomList<int>() { 3, 4, 5 };
+            CustomList<int> listC = new CustomList<int>() { 6, 7, 8 };
+            CustomList<int> aggregateList = new CustomList<int>();
+            CustomList<int> threesList = new CustomList<int>();
+
+            aggregateList = listA + listB + listC;
+            Console.Write(aggregateList);
+            int a = aggregateList[0];
+            int b = aggregateList[3];
+            int c = aggregateList[6];
+            threesList.Add(a);
+            threesList.Add(b);
+            threesList.Add(c);
+
+            int result = threesList[1];
+            int expected = 3;
+
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void Minus_Overload_CheckValueAtIndex()
+        {
+            CustomList<int> list = new CustomList<int>() { 1, 2, 3, 4, 5 };
+            CustomList<int> listB = new CustomList<int>() { 2, 3 };
+
+            int expectedValue = 5;
+            int resultingValue;
+
+            list = list - listB;
+
+            resultingValue = list[2];
+
+            Assert.AreEqual(expectedValue, resultingValue);
+        }
+
+        [TestMethod]
+        public void Zip_CheckValueAtIndex()
+        {
+            CustomList<int> list = new CustomList<int>() { 1, 2, 3, 4, 5 };
+            CustomList<int> itemsAdded = new CustomList<int>() { 0, 0, 0, };
+            CustomList<int> zippedList = new CustomList<int>();
+            int expectedValue = 0;
+            int result;
+
+            zippedList = list.Zip(itemsAdded);
+
+            result = zippedList[1];
+
+            Assert.AreEqual(expectedValue, result);
+        }
+
+        [TestMethod]
+        public void Zip_CheckCount()
+        {
+            CustomList<int> list = new CustomList<int>() { 1, 2, 3, 4, 5 };
+            CustomList<int> itemsAdded = new CustomList<int>() { 0, 0, 0, };
+            CustomList<int> zippedList = new CustomList<int>();
+            int expectedCount = 6;
+            int result;
+
+            zippedList = list.Zip(itemsAdded);
+
+            result = zippedList.Count;
+
+            Assert.AreEqual(expectedCount, result);
+        }
+
+    }
+}
